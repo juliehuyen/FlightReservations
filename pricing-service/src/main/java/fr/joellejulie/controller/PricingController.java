be8fc17b0ffdd422dcd41ac60207cc3ea06adc6b
Joëlle <joelle.huyen13@hotmail.fr>
2025-06-17 21:54:10 +0200

    🚑️ Fix pricing-service

--- pricing-service/src/main/java/fr/joellejulie/controller/PricingController.java
+++ pricing-service/src/main/java/fr/joellejulie/controller/PricingController.java
@@ -1,24 +1,38 @@
 package fr.joellejulie.controller;
 
+import fr.joellejulie.dto.PricingDto;
+import fr.joellejulie.entity.Pricing;
 import fr.joellejulie.service.PricingService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
-@RequestMapping("/v1/pricing")
+@RequestMapping("/v1/pricings")
 @AllArgsConstructor
 public class PricingController {
 
     private final PricingService pricingService;
 
-    @GetMapping("/{flightId}/{date}")
-    public Float getPrice(@PathVariable Long flightId, @PathVariable LocalDate date) {
-        return pricingService.getPrice(flightId, date);
+    @GetMapping("/flights")
+    public ResponseEntity<Float> getPrice(@RequestParam Long flightId, @RequestParam LocalDate date) {
+        Float price = pricingService.getPrice(flightId, date);
+        if (price < 0) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(price);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PricingDto>> getAllPricings() {
+        List<Pricing> pricings = pricingService.getAllPrices();
+        List<PricingDto> pricingDtos = pricings.stream()
+                .map(PricingDto::mapToDTO)
+                .toList();
+        return ResponseEntity.ok(pricingDtos);
     }
 
 }
--- pricing-service/src/main/java/fr/joellejulie/service/impl/PricingServiceImpl.java
+++ pricing-service/src/main/java/fr/joellejulie/service/impl/PricingServiceImpl.java
@@ -1,5 +1,8 @@
 package fr.joellejulie.service.impl;
 
+import fr.joellejulie.client.FlightClient;
+import fr.joellejulie.dto.FlightDto;
+import fr.joellejulie.dto.PricingDto;
 import fr.joellejulie.entity.Pricing;
 import fr.joellejulie.repository.PricingRepository;
 import fr.joellejulie.service.PricingService;
@@ -7,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -14,14 +18,22 @@ public class PricingServiceImpl implements PricingService {
 
     private final PricingRepository pricingRepository;
 
+    private final FlightClient flightClient;
+
     @Override
     public Float getPrice(Long flightId, LocalDate date) {
-        Pricing pricing = pricingRepository.findByFlightIdAndDate(flightId, date).orElse(null);
+        FlightDto flight = flightClient.getFlightById(flightId);
+        Pricing pricing = pricingRepository.findByFlightIdAndDate(flight.getId(), date).orElse(null);
         if (pricing == null) {
             return -1f;
         } else {
             return pricing.getPrice();
         }
+    }
+
+    @Override
+    public List<Pricing> getAllPrices() {
+        return pricingRepository.findAll();
     }
 
 }
