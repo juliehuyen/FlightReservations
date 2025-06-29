a2855e17f91521819edd6bcadd7510e23093d6ec
Julie <juliehuyen@outlook.fr>
2025-06-29 22:44:31 +0200

    ✅ Tests unitaires - Flight Service

---
+++ flight-service/src/test/java/fr/joellejulie/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.joellejulie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FlightServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
